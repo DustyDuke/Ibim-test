23f25568a64ab53a58d58391e2d0f69abc514cb8
Lena <kotenevaelena13@g.mail.com>
2020-06-03 21:25:30 +0300

    add database

---
+++ modal.js
@@ -0,0 +1,76 @@
+function _createModal(options){
+	const DEFAULT_WIDTH = '600px'
+	const modal = document.createElement('div')
+	modal.classList.add('wmodal')
+	modal.insertAdjacentHTML('afterbegin', `
+<div class="modal-overlay"  data-close='true'>
+<div class="modal-window">
+<span class="modal-close" data-close='true'>&times;</span>
+<div class="modal-header">
+	
+<h2 class="modal-title" data-title></h2>
+</div>
+<div class="modal-body" data-content>
+</div>
+</div>
+</div>
+`)
+
+document.body.appendChild(modal)
+return modal
+}
+
+$.modal = function(options){
+	const ANMATION_SPEED = 200
+	const $modal = _createModal(options)
+	let closing = false
+	let destroyed = false
+	const modal = {
+		open() {
+			if(destroyed){
+				return console.log('Modal is destroyed')
+			}
+			!closing && $modal.classList.add('open')
+		},
+		close() {
+			closing = true
+			$modal.classList.remove('open')
+			$modal.classList.add('hide')
+			setTimeout(() => {
+			$modal.classList.remove('hide')
+			closing = false
+			}, ANMATION_SPEED )
+		},
+		destroy() {
+			$modal.parentNode.removeChild($modal)
+			$modal.removeEventListener('click', listener)
+			destroyed = true
+			}
+	}	
+	const listener = event => {
+		if(event.target.dataset.close){
+			modal.close()
+			setTimeout(() => modal.destroy(), 1000)
+		}
+	}
+	
+	$modal.addEventListener('click', listener)
+	
+	return Object.assign(modal, {
+		
+			setTitle(html){
+			$modal.querySelector('[data-title]').innerHTML = html
+		},
+		setContent(html){
+			$modal.querySelector('[data-content]').innerHTML = html
+		}
+	})
+}
+
+document.addEventListener('click', event => {
+	const btnType = event.target.dataset.btn
+	const id = +event.target.dataset.id
+	
+
+		setTimeout(() => modal.open(), 200)
+	})
---
+++ scripts.js
@@ -0,0 +1,82 @@
+let users = [
+{id: '3', name: 'Bendi Manos', age: 20},
+{id: '2', name: 'Al Rayhan', age: 40},
+{id: '1', name: 'Ali Sayed', age: 13},
+{id: '4', name: 'Ali Sayeda', age: 20}
+]
+
+console.log(users)
+
+const toUsersTable = user => `
+<tr><td>${user.id}</td><td>${user.name}</td><td>${user.age}</td><td><button class="removeButton">&times;</button></td></tr>
+`
+
+
+function render() {
+
+const htmlTable = users.map(user => toUsersTable(user)).join('')
+document.querySelector('table').insertAdjacentHTML('beforeEnd', htmlTable)
+}
+
+render()
+
+
+let tbody = document.querySelectorAll('tBody')[0]
+let row = Array.from(document.querySelectorAll('tr'))
+function SortAge(){
+let sortAge = row.slice(1).sort((rowA, rowB) => rowA.cells[2].innerHTML > rowB.cells[2].innerHTML ? 1 : -1)
+  tbody.append(...sortAge)
+}
+
+function SortSurname(){
+let sortSurname = row.slice(1).sort((rowA, rowB) =>  rowA.cells[1].innerHTML > rowB.cells[1].innerHTML ? 1 : -1)
+tbody.append(...sortSurname)
+
+}
+
+function SortName(){
+	let sortName = row.slice(1).sort((rowA, rowB) => rowA.cells[1].innerHTML.split(' ')[1] < rowB.cells[1].innerHTML.split(' ')[1]  ? 1 : -1)
+	console.log(sortName)
+	tbody.append(...sortName)
+
+}
+
+table.onclick = function(event) {
+      if (event.target.className != 'removeButton') return;
+	let removeRow = event.target.closest('tr')
+    removeRow.remove()
+}
+
+
+function AddUser(){
+let addForm = document.createElement('div')
+addForm.insertAdjacentHTML('afterbegin', `
+	<div class="modal-overlay"  data-close='true'>
+<div class="modal-window">
+<span class="modal-close" data-close='true'>&times;</span>
+<div class="modal-header">
+	
+<h2 class="modal-title">Добавить пользователя</h2>
+</div>
+<div class="modal-body" data-content>
+	<form method="post" onsubmit="alert('submit!')">
+<input type="text" placeholder="Введите ID" value="" />
+<input type="text" placeholder="Введите ФИО"  value="" />
+<input type="text" placeholder="Введите возраст" value="" />
+<input type="submit" value="Добавить">
+</form>
+<div class="modal-body" data-content>
+</div>
+</div>
+</div>
+`)
+document.body.appendChild(addForm)
+}
+
+function addData(){
+let inputs = Array.from(document.querySelectorAll('input'))
+
+console.log(inputs)
+}
+AddUser()
+addData()
